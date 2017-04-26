3f1674100d4ef10706cc697301a4a8fce1910125
Kornelia Szabo <kszabo@thoughtworks.com>
2017-04-26 11:19:55 +0100

    Creating erb files out of html

--- helpers.rb
+++ helpers.rb
@@ -2,4 +2,8 @@ helpers do
   def render_view(path, options={})
     erb(path.to_sym, options)
   end
+
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
 end
--- sinatra.rb
+++ sinatra.rb
@@ -11,6 +11,37 @@ get '/' do
   render_view :index
 end
 
+get '/contact' do
+  @page_title = "Explore California: Contact us"
+  render_view :contact
+end
+
+get '/explorers' do
+  @page_title = "Explore California: Explorers"
+  render_view :explorers
+end
+
+get '/mission' do
+  @page_title = "Explore California: Mission"
+  render_view :mission
+end
+
+get 'resources' do
+  @page_title = "Explore California: Resources"
+  render_view :resources
+end
+
+get '/support' do
+  @page_title = "Explore California: Support"
+  render_view :support
+end
+
+get '/tours' do
+  @page_title = "Explore California: Tours"
+  @h1_title = "<script>alert('Gotcha');></script>"
+  erb :tours
+end
+
 get '/test' do
   template = "<h1>The current year is <%= Time.now.year %></h1> <h2>Exact time is: <%= Time.now %>"
   render_view template
